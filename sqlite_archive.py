efea3299692adec0ac4192dce1a3a08044b4ea91
Andrew Nelson <evildarkarchon@gmail.com>
2019-05-07 02:52:29 -0700

    Hopefully prevent null entries in the duplicates json

--- sqlite_archive.py
+++ sqlite_archive.py
@@ -113,13 +113,13 @@ class SQLiteArchive:
                     raise
                 print("duplicate")
                 
-                query = self.dbcon.execute("select filename from {} where hash == ? and not filename == ?".format(args.table), (digest, name)).fetchall()
+                query = self.dbcon.execute("select filename from {} where hash == ?".format(args.table), (digest)).fetchall()
                 if args.fulldups and type(query) is list and len(query) >= 1:
-                    dups[str(fullpath)] = query[0]
+                    if query[0] is not None:
+                        dups[str(fullpath)] = query[0]
                 elif not args.fulldups and type(query) is list and len(query) >= 1:
-                    dups[relparent] = query[0]
-                else:
-                    dups[relparent] = None
+                    if query[0] is not None:
+                        dups[relparent] = query[0]
                 
                 #if args.debug:
                 #    exctype, value = sys.exc_info()[:2]
