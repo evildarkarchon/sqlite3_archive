09348d4b55af2221f24cb4462dbe8d24be602134
Andrew Nelson <evildarkarchon@gmail.com>
2019-08-06 18:52:30 -0700

    Multiple argparse and formatting changes, add ability to select autovacuum mode. Revert while loop in dup processing.

--- sqlite_archive.py
+++ sqlite_archive.py
@@ -14,11 +14,13 @@ from dataclasses import dataclass
 from typing import Any, Dict, List, Tuple
 
 files_args: Tuple = ("files", "*")
-lowercase_table_args: Tuple = (
-    "--lowercase-table", "store_true", "lower",
-    "Modify the inferred table name to be lowercase (has no effect if table name is specified)."
-)
-table_arguments: Tuple = ("--table", "-t", "Name of table to use.", "table")
+lowercase_table_args: Dict = {"long": "--lowercase-table", "action": "store_true", "dest": "lower",
+                              "help": "Modify the inferred table name to be lowercase (has no effect if table name is specified)."}
+table_arguments: Dict = {"long": "--table", "short": "-t", "dest": "table", "help": "Name of table to use."}
+autovacuum_args: Dict = {"long": "--autovacuum-mode", "short": "-a", "nargs": 1, "dest": "autovacuum",
+                         "choices": [0, 1, 2, 'disabled', 'disable', 'enabled', 'enable', 'full', 'intermediate'],
+                         "default": "full",
+                         "help": "Sets the automatic vacuum mode.", "default": "full"}
 
 parser: argparse.ArgumentParser = argparse.ArgumentParser(
     description="Imports or Exports files from an sqlite3 database.")
@@ -33,21 +35,19 @@ parser.add_argument("--debug",
                     dest="debug",
                     action="store_true",
                     help="Supress any exception skipping and print some additional info.")
-parser.add_argument(
-    "--verbose",
-    "-v",
-    action="store_true",
-    help="Print some more information without changing the exception raising policy."
-)
+parser.add_argument("--verbose",
+                    "-v",
+                    action="store_true",
+                    help="Print some more information without changing the exception raising policy."
+                    )
 walargs = parser.add_mutually_exclusive_group()
 walargs.add_argument("--wal", "-w", action="store_true", dest="wal", help="Use Write-Ahead Logging instead of rollback journal.")
-walargs.add_argument("--rollback", "-r", action="store_true", dest="rollback", help="Switch back to rollback journal instead of Write-Ahead Logging.")
+walargs.add_argument("--rollback", "-r", action="store_true", dest="rollback", help="Switch back to rollback journal if Write-Ahead Logging is active.")
 
-drop: argparse.ArgumentParser = subparsers.add_parser(
-    'drop',
-    aliases=['drop-table', 'drop_table'],
-    help="Drop the specified table. NOTE: this will run VACUUM when done, by default."
-)
+drop: argparse.ArgumentParser = subparsers.add_parser('drop',
+                                                      aliases=['drop-table', 'drop_table'],
+                                                      help="Drop the specified table. NOTE: this will run VACUUM when done, by default."
+                                                      )
 drop.add_argument("--no-drop-vacuum",
                   action="store_false",
                   dest="drop_vacuum",
@@ -55,33 +55,32 @@ drop.add_argument("--no-drop-vacuum",
 drop.add_argument("table", help="Name of table to use")
 
 add = subparsers.add_parser("add", help="Add files to the database.")
-add.add_argument(table_arguments[0],
-                 table_arguments[1],
-                 dest=table_arguments[3],
+add.add_argument(autovacuum_args["long"], autovacuum_args["short"], default=autovacuum_args["default"],
+                 dest=autovacuum_args["dest"], choices=autovacuum_args["choices"], help=autovacuum_args["help"])
+add.add_argument(table_arguments["long"],
+                 table_arguments["short"],
+                 dest=table_arguments["dest"],
                  type=str,
-                 help=table_arguments[2])
-add.add_argument(
-    "--replace",
-    "-r",
-    action="store_true",
-    help="Replace any existing file entry's data instead of skipping. By default, the VACUUM command will be run to prevent database fragmentation."
-)
+                 help=table_arguments["help"])
+add.add_argument("--replace",
+                 "-r",
+                 action="store_true",
+                 help="Replace any existing file entry's data instead of skipping. By default, the VACUUM command will be run to prevent database fragmentation."
+                 )
 add.add_argument("--no-replace-vacuum",
                  action="store_false",
                  dest="replace_vacuum",
                  help="Do not run the VACUUM command after replacing data.")
-add.add_argument(
-    "--dups-file",
-    type=str,
-    dest="dups_file",
-    help="Location of the file to store the list of duplicate files to. Defaults to duplicates.json in current directory.",
-    default=f"{pathlib.Path.cwd()}/duplicates.json")
-add.add_argument(
-    "--no-dups",
-    action="store_false",
-    dest="dups",
-    help="Disables saving the duplicate list as a json file or reading an existing one from an existing file."
-)
+add.add_argument("--dups-file",
+                 type=str,
+                 dest="dups_file",
+                 help="Location of the file to store the list of duplicate files to. Defaults to duplicates.json in current directory.",
+                 default=f"{pathlib.Path.cwd()}/duplicates.json")
+add.add_argument("--no-dups",
+                 action="store_false",
+                 dest="dups",
+                 help="Disables saving the duplicate list as a json file or reading an existing one from an existing file."
+                 )
 add.add_argument("--hide-dups",
                  dest="hidedups",
                  action="store_true",
@@ -90,61 +89,57 @@ add.add_argument("--dups-current-db",
                  dest="dupscurrent",
                  action="store_true",
                  help="Only show the duplicates from the current database.")
-add.add_argument(lowercase_table_args[0],
-                 action=lowercase_table_args[1],
-                 dest=lowercase_table_args[2],
-                 help=lowercase_table_args[3])
-add.add_argument(
-    "--no-atomic",
-    action="store_false",
-    dest="atomic",
-    help="Run commit on every insert instead of at the end of the loop.")
+add.add_argument(lowercase_table_args["long"],
+                 action=lowercase_table_args["action"],
+                 dest=lowercase_table_args["dest"],
+                 help=lowercase_table_args["help"])
+add.add_argument("--no-atomic",
+                 action="store_false",
+                 dest="atomic",
+                 help="Run commit on every insert instead of at the end of the loop.")
 add.add_argument(files_args[0],
                  nargs=files_args[1],
                  help="Files to be archived in the SQLite Database.")
 
-compact = subparsers.add_parser(
-    "compact",
-    help="Run the VACUUM query on the database (WARNING: depending on the size of the DB, it might take a while)"
-)
+compact = subparsers.add_parser("compact",
+                                help="Run the VACUUM query on the database (WARNING: depending on the size of the DB, it might take a while)")
 
-create = subparsers.add_parser(
-    "create",
-    aliases=['create-table', 'create_table'],
-    help="Runs the table creation queries and exits.")
-create.add_argument("table", help=table_arguments[2])
+create = subparsers.add_parser("create",
+                               aliases=['create-table', 'create_table'],
+                               help="Runs the table creation queries and exits.")
+create.add_argument(autovacuum_args["long"], autovacuum_args["short"], nargs=autovacuum_args["nargs"],
+                    default=autovacuum_args["default"],
+                    dest=autovacuum_args["dest"], choices=autovacuum_args["choices"],
+                    help=autovacuum_args["help"])
+create.add_argument("table", help=table_arguments["help"])
 
-extract: argparse.ArgumentParser = subparsers.add_parser(
-    'extract', help="Extract files from a table instead of adding them.")
-extract.add_argument(table_arguments[0],
-                     table_arguments[1],
-                     dest=table_arguments[3],
+extract: argparse.ArgumentParser = subparsers.add_parser('extract', help="Extract files from a table instead of adding them.")
+extract.add_argument(table_arguments["long"],
+                     table_arguments["short"],
+                     dest=table_arguments["dest"],
                      type=str,
-                     help=table_arguments[2])
-extract.add_argument(
-    "--output-dir",
-    "-o",
-    dest="out",
-    type=str,
-    help="Directory to output files to. Defaults to a directory named after the table in the current directory."
-)
-extract.add_argument(lowercase_table_args[0],
-                     action=lowercase_table_args[1],
-                     dest=lowercase_table_args[2],
-                     help=lowercase_table_args[3])
-extract.add_argument(
-    "--force",
-    "-f",
-    dest="force",
-    action="store_true",
-    help="Forces extraction of a file from the database, even if the digest of the data does not match the one recorded in the database."
-)
-extract.add_argument(
-    "--infer-pop-file",
-    action="store_true",
-    dest="pop",
-    help="Removes the first entry in the file list when inferring the table name (has no effect when table name is specified)."
-)
+                     help=table_arguments["help"])
+extract.add_argument("--output-dir",
+                     "-o",
+                     dest="out",
+                     type=str,
+                     help="Directory to output files to. Defaults to a directory named after the table in the current directory."
+                     )
+extract.add_argument(lowercase_table_args["long"],
+                     action=lowercase_table_args["action"],
+                     dest=lowercase_table_args["dest"],
+                     help=lowercase_table_args["help"])
+extract.add_argument("--force",
+                     "-f",
+                     dest="force",
+                     action="store_true",
+                     help="Forces extraction of a file from the database, even if the digest of the data does not match the one recorded in the database."
+                     )
+extract.add_argument("--infer-pop-file",
+                     action="store_true",
+                     dest="pop",
+                     help="Removes the first entry in the file list when inferring the table name (has no effect when table name is specified)."
+                     )
 extract.add_argument(files_args[0],
                      nargs=files_args[1],
                      help="Files to be extracted from the SQLite Database.")
@@ -170,7 +165,8 @@ if "table" in args and args.table:
 
 
 def infertable():
-    base: pathlib.Path = pathlib.Path(args.files[0]).resolve()
+    if args.mode == "add":
+        base: pathlib.Path = pathlib.Path(args.files[0]).resolve()
 
     if not base.exists():
         return None
@@ -304,20 +300,52 @@ class SQLiteArchive:
                 self.dbcon.execute("PRAGMA journal_mode=delete;")
             except sqlite3.DatabaseError:
                 pass
+
+        def setav():
+            avstate = self.dbcon.execute("PRAGMA auto_vacuum;").fetchone()[0]
+            if args.verbose or args.debug:
+                print(f"current autovacuum mode: {avstate}")
+            if args.autovacuum in ("enable", "enabled", "full", 1, "1") and not avstate == 1:
+                self.dbcon.execute("PRAGMA auto_vacuum = 1;")
+                if args.verbose or args.debug:
+                    print("full auto_vacuum")
+            elif args.autovacuum in ("intermediate", 2, "2") and not avstate == 2:
+                self.dbcon.execute("PRAGMA auto_vacuum = 2;")
+                if args.verbose or args.debug:
+                    print("intermediate auto_vacuum")
+            elif args.autovacuum in ("disable", "disabled", 0, "0") and not avstate == 0:
+                self.dbcon.execute("PRAGMA auto_vacuum = 0;")
+                if args.verbose or args.debug:
+                    print("auto_vacuum disabled")
+            else:
+                if not avstate == 1:
+                    self.dbcon.execute("PRAGMA auto_vacuum = 1;")
+
+        addorcreate = args.mode in ("add", "create")
         if self.db.is_file():
             self.dbcon: sqlite3.Connection = sqlite3.connect(self.db)
-            if not (self.dbcon.execute("PRAGMA auto_vacuum;").fetchone()[0]
-                    ) == 1 and args.mode in ("add", "create"):
-                self.dbcon.execute("PRAGMA auto_vacuum = 1;")
-                if "wal" in args and args.wal and self.dbcon.execute("PRAGMA journal_mode").fetchone()[0] not in ("WAL", "wal", "Wal", "WAl"):
-                    setwal()
-                if "rollback" in args and args.rollback and self.dbcon.execute("PRAGMA journal_mode").fetchone()[0] not in ("delete", "Delete", "DELETE"):
-                    setdel()
+            needsvacuum = False
+            if addorcreate and "autovacuum" in args and args.autovacuum:
+                setav()
+                if not needsvacuum:
+                    needsvacuum = True
+
+            if addorcreate and "wal" in args and args.wal and self.dbcon.execute("PRAGMA journal_mode").fetchone()[0] not in ("WAL", "wal", "Wal", "WAl"):
+                setwal()
+                if not needsvacuum:
+                    needsvacuum = True
+            if addorcreate and "rollback" in args and args.rollback and self.dbcon.execute("PRAGMA journal_mode").fetchone()[0] not in ("delete", "Delete", "DELETE"):
+                setdel()
+                if not needsvacuum:
+                    needsvacuum = True
+
+            if needsvacuum:
                 self.dbcon.execute("VACUUM;")
-        elif not self.db.is_file() and args.mode in ("add", "create"):
+        elif not self.db.is_file() and addorcreate:
             self.db.touch()
             self.dbcon: sqlite3.Connection = sqlite3.connect(self.db)
-            self.dbcon.execute("PRAGMA auto_vacuum = 1;")
+            if "autovacuum" in args and args.autovacuum:
+                setav()
             if "wal" in args and args.wal and self.dbcon.execute("PRAGMA journal_mode").fetchone()[0] not in ("WAL", "wal", "Wal", "WAl"):
                 setwal()
             if "rollback" in args and args.rollback and self.dbcon.execute("PRAGMA journal_mode").fetchone()[0] not in ("delete", "Delete", "DELETE"):
@@ -333,8 +361,7 @@ class SQLiteArchive:
         atexit.register(self.dbcon.close)
         atexit.register(self.dbcon.execute, "PRAGMA optimize;")
 
-        if args.mode in ("add", "extract") and 'files' in args and len(
-                args.files) > 0:
+        if args.mode in ("add", "extract") and 'files' in args and len(args.files) > 0:
             self.files = [x for x in globlist(args.files) if pathlib.Path(x).resolve() != pathlib.Path(args.db).resolve() and x.is_file()]
             self.files.sort()
             if args.debug or args.verbose:
@@ -523,18 +550,8 @@ class SQLiteArchive:
                     else:
                         insert()
             except sqlite3.IntegrityError:
-
-                query = self.execquerynocommit(
-                    f"select filename from {args.table} where hash == ?",
-                    (fileinfo.digest, ))  # [0][0]
-                try:
-                    while isinstance(query, Iterable) and len(query) == 1:
-                        if type(query[0]) in (str, int, float, sqlite3.Row):
-                            break
-                        else:
-                            query = query[0]
-                except TypeError:
-                    pass
+                query = self.execquerynocommit(f"select filename from {args.table} where hash == ?",
+                                               (fileinfo.digest, ))[0][0]
                 querytype = type(query)
                 querylen = len(query)
                 if args.debug or args.verbose:
@@ -546,14 +563,7 @@ class SQLiteArchive:
                         print(query)
                 if query and querylen >= 1:
                     print("duplicate")
-                if not type(query) == str and querylen >= 1:
-                    for i in query:
-                        i = i["filename"]
-                        if args.verbose or args.debug:
-                            print(i)
-                        if len(i) >= 1:
-                            dups[dbname][str(fullpath)] = str(i)
-                elif type(query) == str and querylen >= 1:
+
                     dups[dbname][str(fullpath)] = str(query)
                     if args.debug or args.verbose:
                         print(query)
@@ -564,11 +574,8 @@ class SQLiteArchive:
                     except KeyError:
                         pass
                 for z in tuple(dups[dbname].keys()):
-                    if not querytype == str:
-                        _query = str(query)
-                        if _query in z:
-                            removefromdict()
-                    elif querytype == str and query in z:
+                    query = str(pathlib.Path(str(query)).resolve())
+                    if query in z:
                         removefromdict()
 
                 if args.debug:
